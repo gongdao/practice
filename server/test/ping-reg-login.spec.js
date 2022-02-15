201e75a8ebcaa7d0694051f36ed1f7ee2405bd96
gongdao <gongdao@users.noreply.github.com>
2022-02-16 01:55:17 +0800

    Added ping-reg-login.spec.js and profile spec.js

--- server/test/ping-reg-login.spec.js
+++ server/test/ping-reg-login.spec.js
@@ -7,38 +7,52 @@ const should = require('should');
 chai.use(chaiHttp);
 
 describe("/POST ping", () => {
-  // it("it should return 200 and message", (done) => {
-  //   chai
-  //     .request.agent('http://localhost:3001')
-  //     .post(`/auth/logout`)
-  //     .end((err, res) => {
-  //       res.text.should.have
-  //         .eql("You have successfully logged out");
-  //       done();
-  //     });
-  // });
+  it("it should return 200 and message", (done) => {
+    chai
+      .request.agent('http://localhost:3001')
+      .post(`/auth/logout`)
+      .end((err, res) => {
+        res.text.should.have
+          .eql("You have successfully logged out");
+        done();
+      });
+  });
   // it('should create a new user', (done) => {
   //   chai
   //     .request('http://localhost:3001')
   //     .post('/auth/register')
-  //     .send({username: 'Bob', email: 'bob@yahoo.ca', password: 'password'})
+  //     .send({username: 'Susan', email: 'susan@yahoo.ca', password: 'password'})
   //     .end((err, res) => {
   //       chai.expect(res.status).to.equal(201)
   //       done()
   //     })
   // })
  
-  // let cookies;
-  // it('should login successfully', (done) => {
-  //   chai
-  //     .request('http://localhost:3001')
-  //     .post('/auth/login')
-  //     .send({ email: 'bob@yahoo.ca', password: 'password'})
-  //     .end((err, res) => {
-  //       chai.expect(res.status).to.equal(200)
-  //       cookies = res.headers['set-cookie'].pop().split(';')[0];
-  //       console.log(cookies)
-  //       done()
-  //   })
-  // })
+  let cookies;
+  it('should login successfully', (done) => {
+    chai
+      .request('http://localhost:3001')
+      .post('/auth/login')
+      .send({ email: 'susan@yahoo.ca', password: 'password'})
+      .end((err, res) => {
+        chai.expect(res.status).to.equal(200)
+        cookies = res.headers['set-cookie'].pop().split(';')[0];
+        console.log(cookies)
+        done()
+    })
+  })
+  
+  it('should get a user data', (done) => {
+    chai
+      .request('http://localhost:3001')
+      .get('/auth/user/')
+      .set('Cookie', cookies)
+      .end((err, res) => {
+        chai.expect(res.status).to.equal(200)
+        res.body.success.user.username.should.have
+          .eql("Susan");
+        console.log(res.body.success.user)
+        done()
+    })
+  })
 });
--- server/test/profile.spec.js
+++ server/test/profile.spec.js
@@ -22,27 +22,27 @@ describe("/Profile CRUD", () => {
       })
   })
   
-    // it('should create a profile successfully', (done) => {
-    //   const u = {
-    //     user: "61f58f2574d7fe4e5fd7ea70",
-    //     firstname: "bob",
-    //     lastname: "joe",
-    //     description: "no",
-    //     availability: "now",
-    //     telephone: "1-416-222-1234",
-    //     address: "123 Kennedy Road",
-    //     imgurl:"http://img.com/hatchways/001"
-    //   }
-    //   chai
-    //     .request('http://localhost:3001')
-    //     .post('/profile/create')
-    //     .send(u)
-    //     .set('Cookie', cookies)
-    //     .end((err, res) => {
-    //       chai.expect(res.status).to.equal(201)
-    //       done()
-    //   })
-    // })
+    it('should create a profile successfully', (done) => {
+      const u = {
+        user: "61f58f2574d7fe4e5fd7ea70",
+        firstname: "bob",
+        lastname: "joe",
+        description: "no",
+        availability: "now",
+        telephone: "1-416-222-1234",
+        address: "123 Kennedy Road",
+        imgurl:"http://img.com/hatchways/001"
+      }
+      chai
+        .request('http://localhost:3001')
+        .post('/profile/create')
+        .send(u)
+        .set('Cookie', cookies)
+        .end((err, res) => {
+          chai.expect(res.status).to.equal(201)
+          done()
+      })
+    })
   
   it('should get user session for current user', (done) => {
     var req = request('http://localhost:3001').get('/auth/login');
@@ -56,17 +56,17 @@ describe("/Profile CRUD", () => {
       });
   })
 
-  // it('should get a profile by id successfully', (done) => {
-  //   chai
-  //     .request('http://localhost:3001')
-  //     .get('/profile/mid/' + '61d8ff1364de7403313f3163')
-  //     .set('Cookie', cookies)
-  //     .end((err, res) => {
-  //       chai.expect(res.status).to.equal(200)
-  //       //console.log(res.body)
-  //       done()
-  //   })
-  // })
+  it('should get a profile by id successfully', (done) => {
+    chai
+      .request('http://localhost:3001')
+      .get('/profile/mid/' + '61d8ff1364de7403313f3163')
+      .set('Cookie', cookies)
+      .end((err, res) => {
+        chai.expect(res.status).to.equal(200)
+        //console.log(res.body)
+        done()
+    })
+  })
 
   it('should get a profile by email successfully', (done) => {
     chai
@@ -80,51 +80,51 @@ describe("/Profile CRUD", () => {
     })
   })
 
-  // it('should modify a profile by id successfully', (done) => {
-  //   const u = {
-  //     user: "61d8ff1364de7403313f3163",
-  //     firstname: "bob",
-  //     lastname: "joe",
-  //     description: "no",
-  //     availability: "now",
-  //     telephone: "1-416-222-1234",
-  //     address: "123 Kennedy Road",
-  //     imgurl:"http://img.com/hatchways/003"
-  //   }
-  //   chai
-  //     .request('http://localhost:3001')
-  //     .put('/profile/mid/' + '61f58f2574d7fe4e5fd7ea70')
-  //     .send(u)
-  //     .set('Cookie', cookies)
-  //     .end((err, res) => {
-  //       chai.expect(res.status).to.equal(200)
-  //       //console.log(res.body)
-  //       done()
-  //   })
-  // })
+  it('should modify a profile by id successfully', (done) => {
+    const u = {
+      user: "61d8ff1364de7403313f3163",
+      firstname: "bob",
+      lastname: "joe",
+      description: "no",
+      availability: "now",
+      telephone: "1-416-222-1234",
+      address: "123 Kennedy Road",
+      imgurl:"http://img.com/hatchways/003"
+    }
+    chai
+      .request('http://localhost:3001')
+      .put('/profile/mid/' + '61f58f2574d7fe4e5fd7ea70')
+      .send(u)
+      .set('Cookie', cookies)
+      .end((err, res) => {
+        chai.expect(res.status).to.equal(200)
+        //console.log(res.body)
+        done()
+    })
+  })
 
-  // it('should modify a profile by email successfully', (done) => {
-  //   const u = {
-  //     user: "61d8ff1364de7403313f3163",
-  //     firstname: "bob",
-  //     lastname: "joe",
-  //     description: "no",
-  //     availability: "now",
-  //     telephone: "1-416-222-1234",
-  //     address: "123 Kennedy Road",
-  //     imgurl:"http://img.com/hatchways/0034"
-  //   }
-  //   chai
-  //     .request('http://localhost:3001')
-  //     .put('/profile/' + 'robot@yahoo.ca')
-  //     .send(u)
-  //     .set('Cookie', cookies)
-  //     .end((err, res) => {
-  //       chai.expect(res.status).to.equal(200)
-  //       //console.log(res.body)
-  //       done()
-  //   })
-  // })
+  it('should modify a profile by email successfully', (done) => {
+    const u = {
+      user: "61d8ff1364de7403313f3163",
+      firstname: "bob",
+      lastname: "joe",
+      description: "no",
+      availability: "now",
+      telephone: "1-416-222-1234",
+      address: "123 Kennedy Road",
+      imgurl:"http://img.com/hatchways/0034"
+    }
+    chai
+      .request('http://localhost:3001')
+      .put('/profile/' + 'robot@yahoo.ca')
+      .send(u)
+      .set('Cookie', cookies)
+      .end((err, res) => {
+        chai.expect(res.status).to.equal(200)
+        //console.log(res.body)
+        done()
+    })
+  })
 
   // it('should delete a profile by id successfully', (done) => {
   //   const u = {
@@ -147,7 +147,7 @@ describe("/Profile CRUD", () => {
   //       console.log(res.body)
   //       done()
   //   })
-  //})
+  // })
 
   it('should list profile successfully', (done) => {
     chai
